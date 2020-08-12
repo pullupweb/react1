0045381c3d6f6c9425dfcdc033bfa711da494e27
unknown <ensiorprince45@gmail.com>
2020-08-12 01:24:14 -0700

    Updates

---
+++ precache-manifest.f568b0c47fcfb97fc92fbad1668d15d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b563b18e1e35f07263320ee8449f3817",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "7cd1301a8d51a2ae2e73",
+    "url": "/react1/static/css/main.8c27c62f.chunk.css"
+  },
+  {
+    "revision": "aa6984cd07520a83fd66",
+    "url": "/react1/static/js/2.0a57a6d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react1/static/js/2.0a57a6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd1301a8d51a2ae2e73",
+    "url": "/react1/static/js/main.e2de3578.chunk.js"
+  },
+  {
+    "revision": "8e1857d6b99aa8e6b709",
+    "url": "/react1/static/js/runtime-main.2fb08cee.js"
+  },
+  {
+    "revision": "32a34e5e0b37805623fbd80ac71d9b9c",
+    "url": "/react1/static/media/OIPUG9FRDB7.32a34e5e.jpg"
+  },
+  {
+    "revision": "23bc387ff278e7f8d5f1f6c6e5ba59bc",
+    "url": "/react1/static/media/Pop-Smoke-2.23bc387f.jpg"
+  },
+  {
+    "revision": "1e3a922d1e4ca02ae89967d18348ef89",
+    "url": "/react1/static/media/Screen-Shot-2019-08-14-at-3.44.09-PM.1e3a922d.png"
+  }
+]);
